3ef867fa90ddc4b0830e1446695a2a48cb93a0a4
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2025-06-23 15:50:16 -0500

    pause menu :3

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	print("test2")
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	
+func pause():
+	print("test")
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func testEsc():
+	#print("test1")
+	if Input.is_action_just_pressed("escape") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("escape") and get_tree().paused:
+		resume()
+
+
+func _on_resume_button_pressed() -> void:
+	resume()
+	#hide()
+
+func _process(delta: float) -> void:
+	testEsc()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
