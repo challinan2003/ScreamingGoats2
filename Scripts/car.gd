f6c4c51468ecfe2aa6f9aabb317f83e92645634c
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2025-06-24 13:18:32 -0500

    car movement semi final

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -2,7 +2,7 @@ extends VehicleBody3D
 
 var max_rpm = 500
 var max_torque = 200
-
+var mode = 0
 #Gets the path of the headlight object
 @onready var light = get_node("carbody/SpotLight3D")
 
@@ -12,9 +12,17 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right","left") * 0.4,5 * delta)
-	var acceleration = Input.get_axis("back","forward")
+	#var acceleration = Input.get_axis("back","forward")
 	var rpm = $back_left_wheel.get_rpm()
+	var acceleration = mode
+	if mode == 0 && rpm > 0:
+		acceleration = -1
 	$back_left_wheel.engine_force  = acceleration * max_torque * (1 - rpm / max_rpm)
 	rpm = $back_right_wheel.get_rpm()
 	$back_right_wheel.engine_force  = acceleration * max_torque * (1 - rpm / max_rpm)
+	
+	if Input.is_action_just_pressed("forward") && mode <= 2:
+		mode+=1
+	elif Input.is_action_just_pressed("back") && mode > 0:
+		mode-=1
 	
