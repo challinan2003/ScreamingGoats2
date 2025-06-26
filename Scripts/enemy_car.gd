811a741efa1e01fc2b0d75e51d356b2bdd15824e
XUJordano <xavierujordano@gmail.com>
2025-06-26 16:31:56 -0500

    End of game
    My coding's fucking awful but it works.

---
+++ Scripts/enemy_car.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+# Don't ask me why my code is so fucking bad. 
+# This shit is held together with spit and duct tape.
+# I'm like the McDonalds chef of coding.
+var player = null
+var speedz = 100
+var chase = false
+
+func _on_detection_zone_body_entered(body: Node3D) -> void:
+	player = body
+	chase = true
+
+func _process(delta: float) -> void:
+	if chase:
+		position.z += (player.position.z - position.z) / speedz
+		position.x = player.position.x + 2
+	#position.z += .1
+
+func _on_death_zone_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	print("ded")
+	get_tree().reload_current_scene()
---
+++ Scripts/enemy_spawn_trigger.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+# This is my comment to tell you how this code works.
+# I have no fucking clue how this works.
+var should_spawn = true
+var enemy_scene = preload("res://Scenes/Other Scenes n Shit/Enemy_Car.tscn")
+
+# THIS SHIT ISN'T USED. I SPENT LIKE 60 MINUTES TRYING TO FIGURE IT OUT ONLY TO
+# REALIZE IT ISN'T NEEDED. SOMEONE FUCKING KILL ME.
+# @onready var node_position: CollisionShape3D = %SpawnBox
+
+# I could clean this up, but it works and I'm too lazy to.
+func _on_body_entered(body: Node3D):
+	if should_spawn == true:
+		var enemy = enemy_scene.instantiate()
+		# The position and rotation_degrees are relative to the trigger box scene
+		# NOT the game world scene.
+		enemy.position = Vector3(1, 0, -15)
+		enemy.rotation_degrees = Vector3(0, 180, 0)
+		add_child(enemy)
+		# print("spawning enemy")
+		should_spawn = false
+	
