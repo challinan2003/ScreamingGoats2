1cfb479460560b991e15784f35c93b775cb911d5
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2025-06-26 23:03:10 -0500

    eads

--- Scripts/driver.gd
+++ Scripts/driver.gd
@@ -14,22 +14,22 @@ var check = true
 
 func _ready() -> void:
 	tutorial.hide()
+	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(event.relative.y * SENSITIVITY)	
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(0), deg_to_rad(30))
 		rotation.y = clamp(rotation.y, deg_to_rad(-100), deg_to_rad(80))
+		print("test1")
 
 func _process(delta: float) -> void:
 	
 	if !get_tree().paused:
 		#pass
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	if Global.gameStarted == true:
+	if Global.gameStarted == true && check == true:
 		camera.make_current()
-		if check == true:
-			tutorial.show()
-			await get_tree().create_timer(4).timeout
-			tutorial.hide()
-			check = false
+		check = false
+		print("test2")
+		
